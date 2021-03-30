ede963b4d94cf40e8f12056f524c9ce21fda638f
Filippo <Dell@DESKTOP-2751OC0>
2021-03-30 11:28:21 +0200

    Aggiungere i file di progetto.

---
+++ Personale/MostraTabella.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Personale
+{
+    /// <summary>
+    /// Logica di interazione per MostraTabella.xaml
+    /// </summary>
+    public partial class MostraTabella : Window
+    {
+        public MostraTabella()
+        {
+            InitializeComponent();
+        }
+    }
+}
